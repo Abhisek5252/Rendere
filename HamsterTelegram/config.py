001d00d7860182cfef779f1575b0c209fedad3a8
Abhisek5252 <abishek.chettri@globolosysfashion.com>
2025-02-11 23:13:42 +0530

    Update config.py

--- HamsterTelegram/config.py
+++ HamsterTelegram/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # Set this in Render
+    SQLALCHEMY_DATABASE_URI = os.getenv('postgresql://metaverse_user:M7YEsplvu1uRhUvpGo7wliNI3g2kp0jv@dpg-culo0h2n91rc73efttn0-a/metaverse')  # Set this in Render
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'your_secret_key_here')

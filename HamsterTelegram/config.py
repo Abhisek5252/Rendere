e8fe6f1e604c3d97cb61a28f02c2478df733239a
Abhisek5252 <abishek.chettri@globolosysfashion.com>
2025-02-11 23:48:57 +0530

    Delete HamsterTelegram directory

--- HamsterTelegram/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://metaverse_user:M7YEsplvu1uRhUvpGo7wliNI3g2kp0jv@dpg-culo0h2n91rc73efttn0-a/metaverse')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'your_secret_key_here')
--- HamsterTelegram/static/js/splash.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-    const splashScreen = document.querySelector('.splash-screen');
-    setTimeout(() => {
-        splashScreen.style.display = 'none';
-    }, 2000);
-});

015f56ccc4a5dd2acd832b1afeca60e4d8b363a6
Abhisek5252 <abishek.chettri@globolosysfashion.com>
2025-02-11 21:37:11 +0530

    Update models.py

--- HamsterTelegram/models.py
+++ HamsterTelegram/models.py
@@ -1,5 +1,7 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+db = SQLAlchemy()  # Define db here
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
